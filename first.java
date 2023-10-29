8c9e26a8ac13757f635d04ec1a1c70574d3ee51c
Deepak <deepak2603dk@gmail.com>
2023-10-29 18:00:35 +0530

    primitive data types

--- first.java
+++ first.java
@@ -1,5 +1,9 @@
 public class first {
     public static void main(String[] args) {
+
+
+        int num = 5;
+        System.err.println(num);
         System.out.println("Hello Universe");
     }
 }
---
+++ variabledatatypes/primitivetypes.java
@@ -0,0 +1,42 @@
+public class primitivetypes {
+    public static void main(String[] args) {
+
+        // int 
+        int num1 = 1;
+        int num2 = 2;
+        System.err.println(num1+num2);
+
+
+        // float 
+        float fnum1 = 1.5f;
+        float fnum2 = 1.5f;
+        System.err.println(fnum1 + fnum2);
+
+
+        //char
+        char character = 'D';
+        System.err.println(character);
+
+        //string
+        String name = "java";
+        System.err.println(name);
+
+
+        //long
+        long lnum1 = 15555L;
+        long lnum2 = 515555L;
+        System.err.println(lnum1 + lnum2);
+
+
+        //double
+         double dnum1 = 155.55d;
+        double dnum2 = 51584.5555d;
+        System.err.println(dnum1 + dnum2);
+
+
+        //boolean
+        boolean javaisfun = true;
+        System.err.println(javaisfun);
+        
+    }
+}
