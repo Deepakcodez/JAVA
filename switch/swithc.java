31d48c0ceaaafa814796153b736508b3e86f102a
Deepak <deepak2603dk@gmail.com>
2023-11-02 02:09:55 +0530

    switch statement

---
+++ switch/swithc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+package switch;
+
+public class swithc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter lucky number");
+        int num = sc.nextInt();
+
+        switch (num) {
+            case 15:
+                System.out.println("you win washing machine");
+                break;
+            case 18:
+                System.out.println("you win car");
+                break;
+        
+            default:
+            System.out.println("oops try again!!!!!!!");
+                break;
+        }
+    }
+    
+}
