50afe30aa37e0ca2113d9c6a5878fa99f5673ecb
Deepak <deepak2603dk@gmail.com>
2023-11-05 20:27:55 +0530

    Constructor and Constructor overloading

---
+++ oops/Constructor.java
@@ -0,0 +1,37 @@
+class empInfo {
+    public int Id;
+    public String Name;
+
+    // condtructor
+    public empInfo() {
+        Id = 101;
+        Name = "Deepak";
+    }
+
+    // constructor overloading
+    public empInfo(String name) {
+        Id = 102;
+        Name = name;
+    }
+
+    // getter 
+    public void getInfo() {
+
+        System.out.println(Id);
+        System.out.println(Name);
+
+    }
+
+}
+
+public class Constructor {
+
+    public static void main(String[] args) {
+        empInfo emp1 = new empInfo();
+        empInfo emp2 = new empInfo("karan");
+        emp1.getInfo();
+        emp2.getInfo();
+
+    }
+
+}
