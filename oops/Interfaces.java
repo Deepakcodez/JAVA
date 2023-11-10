be95dbb01bfac29f35781a029333dccf28e14f8d
Deepak <deepak2603dk@gmail.com>
2023-11-10 21:06:36 +0530

    default method

--- oops/Interfaces.java
+++ oops/Interfaces.java
@@ -1,29 +1,53 @@
-interface flyable{
-    int a=4;
+interface flyable {
+    int a = 4;
+
     void fly();
 }
-interface swimable{
-    int a =5;
+
+interface basicFeature {
+    default void walk() {
+        System.out.println("can walk");
+
+    }
+
+    default void eat() {
+        System.out.println("can eat");
+    }
+}
+
+interface swimable {
+    int a = 5;
+
     void swim();
 }
-class Bird implements flyable,swimable{
-    int a=2;
-     public void fly(){
+
+class Bird implements flyable, basicFeature, swimable {
+    int a = 2;
+
+    @Override
+    public void fly() {
         System.out.println("bird can fly");
         System.out.println(a);
         System.out.println(swimable.a);
         System.out.println(flyable.a);
-     }
-     public void swim(){
+
+    };
+
+    @Override
+    public void swim() {
         System.out.println("bird can swim");
-     }
+    }
+ 
 }
+
 public class Interfaces {
 
     public static void main(String[] args) {
         Bird bird1 = new Bird();
         bird1.fly();
         bird1.swim();
+        bird1.walk();
+        bird1.eat();
     }
-    
+
 }
